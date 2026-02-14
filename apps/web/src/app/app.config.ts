7cb92bb9f5c68bc17644898801e7655b4c236ad4
Ilya Karashchuk <karashchuk_iv@edu.surgu.ru>
2026-02-15 02:59:22 +0500

    style: format all project

--- apps/web/src/app/app.config.ts
+++ apps/web/src/app/app.config.ts
@@ -1,7 +1,4 @@
-import {
-  ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
-} from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 
