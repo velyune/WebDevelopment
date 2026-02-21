2a7a3b47528d2fe3bd77dfbe7825964cebee505e
afgssxhn <tujhl@inbox.ru>
2026-02-21 17:37:06 +0500

    fix: correct webpack output path for api serve executor

--- apps/api/webpack.config.js
+++ apps/api/webpack.config.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../../dist/apps/api'),
+    path: join(__dirname, 'dist'),
     clean: true,
     ...(process.env.NODE_ENV !== 'production' && {
       devtoolModuleFilenameTemplate: '[absolute-resource-path]',
